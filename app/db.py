7b7ad2b983cde66d5ae1b4de576544eda88f6753
ellinorroman <ellinor.roman@hotmail.com>
2026-04-15 15:34:53 +0300

    lektion 7

--- app/db.py
+++ app/db.py
@@ -1,99 +1,37 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from datetime import date
-from app.db import get_conn, create_schema
+import os, psycopg
 
-app = FastAPI()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-origins = ["*"] # Change to the real front end origin in production
+def get_conn():
+    return psycopg.connect(DATABASE_URL, autocommit=True, row_factory=psycopg.rows.dict_row)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-create_schema()
-
-# datamodell för bokning
-class Booking(BaseModel):
-    guest_id: int
-    room_id: int
-    datefrom: date 
-    dateto: date
-
-# Main route for this API
-@app.get("/")
-def read_root(): 
-    with get_conn() as conn, conn.cursor() as cur:
-        cur.execute("SELECT version() ")
-        result = cur.fetchone()
-
-    return { "msg": f"Hotel API!", "db_status": result }
-
-@app.get("/if/{term}")
-def if_test(term: str):
-    ret_str = "Default message..."
-    if (term == "hello" 
-        or term == "hi" 
-        or term == "greetings"):
-        
-        ret_str = "Hello yourself!"
-    elif (term == "morjens" or term == "hej") and 1 == 0:
-        ret_str = "Hej på dig"
-    else:
-        ret_str = f"vad betyder {term}?"
-
-    return { "msg": ret_str }
-        
-    
-# List all rooms 
-@app.get("/rooms")
-def get_rooms(): 
+def create_schema():
     with get_conn() as conn, conn.cursor() as cur:
         cur.execute("""
-            SELECT * 
-            FROM rooms
-            ORDER BY room_number
+            CREATE TABLE IF NOT EXISTS rooms (
+                id SERIAL PRIMARY KEY,
+                room_number INT NOT NULL,
+                created_at TIMESTAMP DEFAULT now()
+            );       
+            ALTER TABLE rooms ADD COLUMN IF NOT EXISTS room_type VARCHAR;
+            ALTER TABLE rooms ADD COLUMN IF NOT EXISTS price NUMERIC;
+
+
+            CREATE TABLE IF NOT EXISTS guests (
+                id SERIAL PRIMARY KEY,
+                firstname VARCHAR NOT NULL,
+                lastname VARCHAR NOT NULL,
+                created_at TIMESTAMP DEFAULT now(),
+                address VARCHAR
+            );
+                    
+
+            CREATE TABLE IF NOT EXISTS bookings (
+                id SERIAL PRIMARY KEY,
+                guest_id INT REFERENCES guests(id), -- foreign key (främmande nyckel)
+                room_id INT REFERENCES rooms(id),
+                datefrom DATE DEFAULT now(),
+                dateto DATE,
+                info VARCHAR
+            );
         """)
-        rooms = cur.fetchall()
-    return rooms
-
-# Get one room
-@app.get("/rooms/{id}")
-def get_room(id: int):
-    with get_conn() as conn, conn.cursor() as cur:
-        cur.execute("""
-            SELECT * 
-            FROM rooms 
-            WHERE id = %s
-        """, [id]) # list här, tuple används också
-        room = cur.fetchone()
-    return room      
-
-# Create booking
-@app.post("/bookings")
-def create_booking(booking: Booking):
-    with get_conn() as conn, conn.cursor() as cur:
-        cur.execute("""
-            INSERT INTO bookings (
-                guest_id,
-                room_id,
-                datefrom,
-                dateto
-            ) VALUES (
-                %s, %s, %s, %s
-            ) RETURNING id
-        """, (
-            booking.guest_id, 
-            booking.room_id,
-            booking.datefrom,
-            booking.dateto
-        ))
-        new_booking = cur.fetchone()
-    return { "msg": "Booking created!", "id": new_booking['id']}
-
--- app/main.py
+++ app/main.py
@@ -19,14 +19,13 @@ app.add_middleware(
 
 create_schema()
 
-# datamodell för bokning
 class Booking(BaseModel):
     guest_id: int
     room_id: int
     datefrom: date 
     dateto: date
 
-# Main route for this API
+
 @app.get("/")
 def read_root(): 
     with get_conn() as conn, conn.cursor() as cur:
@@ -35,23 +34,6 @@ def read_root():
 
     return { "msg": f"Hotel API!", "db_status": result }
 
-@app.get("/if/{term}")
-def if_test(term: str):
-    ret_str = "Default message..."
-    if (term == "hello" 
-        or term == "hi" 
-        or term == "greetings"):
-        
-        ret_str = "Hello yourself!"
-    elif (term == "morjens" or term == "hej") and 1 == 0:
-        ret_str = "Hej på dig"
-    else:
-        ret_str = f"vad betyder {term}?"
-
-    return { "msg": ret_str }
-        
-    
-# List all rooms 
 @app.get("/rooms")
 def get_rooms(): 
     with get_conn() as conn, conn.cursor() as cur:
@@ -63,7 +45,6 @@ def get_rooms():
         rooms = cur.fetchall()
     return rooms
 
-# Get one room
 @app.get("/rooms/{id}")
 def get_room(id: int):
     with get_conn() as conn, conn.cursor() as cur:
@@ -75,7 +56,20 @@ def get_room(id: int):
         room = cur.fetchone()
     return room      
 
-# Create booking
+
+
+@app.get("/bookings")
+def get_bookings(): 
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT * 
+            FROM bookings
+            ORDER BY id
+        """)
+        bookings = cur.fetchall()
+    return bookings
+
+
 @app.post("/bookings")
 def create_booking(booking: Booking):
     with get_conn() as conn, conn.cursor() as cur:
